1cdc2e576bd9968947257bbb93ad0d8dd09e4199
Vera Markosyan <100492393+Vera11113@users.noreply.github.com>
2025-01-10 15:53:31 +0300

    Add files via upload

---
+++ module 9/module_9_7.py
@@ -0,0 +1,21 @@
+
+def is_prime(func):
+
+    def wrapper(*args, **kwargs):
+        n = func(*args, **kwargs)
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                print('Составное')
+                return n
+            else: continue
+        print('Простое')
+        return n
+
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a+b+c
+
+result = sum_three(2, 3, 6)
+print(result)
