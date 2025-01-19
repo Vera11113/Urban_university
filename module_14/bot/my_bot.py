54603eacc13030aa3bd9ecc9f697db949f6a4834
Vera Markosyan <100492393+Vera11113@users.noreply.github.com>
2025-01-19 18:03:43 +0300

    Add files via upload

---
+++ module_14/bot/my_bot.py
@@ -0,0 +1,82 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from numpy.ma.core import resize
+from api import api
+from keyboards import start_kb, kbi, kbi_buy, products
+from glob import glob
+
+
+
+api = api
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+images = glob(r"*.png")
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start_com(message):
+    await message.answer("Привет! Выбери чем хочешь заняться: ", reply_markup=start_kb)
+
+@dp.message_handler(text = 'Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию: ', reply_markup=kbi)
+
+@dp.message_handler(text = 'Купить')
+async def get_buying_list(message):
+    for i in range(len(images)):
+        with open(images[i], 'rb') as img:
+            await message.answer_photo(img, products[i])
+    await message.answer(text = 'Выберите продукт: ', reply_markup=kbi_buy)
+
+
+@dp.callback_query_handler(text = 'formulas')
+async def form_print(call):
+    await call.message.answer('10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
+    await call.answer()
+
+@dp.callback_query_handler(text = 'calories')
+async def set_age(call):
+    await call.message.answer(f'Введите свой возраст:')
+    await call.answer()
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer(f'Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer(f'Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    calories = 10 * float(data['weight']) + 6.25 * float(data['growth']) + 5 * float(data['age']) - 161
+    await message.answer(f'Ваша норма калорий {round(calories, 2)}')
+    await state.finish()
+
+@dp.callback_query_handler(text = 'product_buying')
+async def product_buying(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+@dp.message_handler(text = 'Информация')
+async def info_def(message):
+    await message.answer('Этот бот считает норму калорий')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
