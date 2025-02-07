cee6d51a5b6fdd68a649e8b1b15e590d0764cd28
Vera11113 <vera.markosyan.99@bk.ru>
2025-02-07 16:12:31 +0300

    add task2 connect

--- UrbanDjango/settings.py
+++ UrbanDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example1'
+    'task2'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'UrbanDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'tamplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
